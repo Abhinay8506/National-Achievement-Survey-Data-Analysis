332508477b87fbb61a53b1badea13811c89e526d
Abhinay Goswami <abhinay.21GCEBAIDS059@galgotiacollege.edu>
2024-05-21 16:55:45 +0530

    Update Descriptive source code.py

--- Descriptive Analysis/Descriptive source code.py
+++ Descriptive Analysis/Descriptive source code.py
@@ -2,42 +2,42 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import os
 
-//Setting dataset absolute path
+#Setting dataset absolute path
 os.chdir(r"C:\Users\user\Downloads")
 
-//Reading Dataset
+#Reading Dataset
 st=pd.read_csv("surveydataset.csv")
 
-//Checking shape means (No. of rows,No. of columns)
+#Checking shape means (No. of rows,No. of columns)
 st.shape
 
-//First 5 rows
+#First 5 rows
 st.head()
 
-//Maximum no. of columns to observe
+#Maximum no. of columns to observe
 pd.set_option('display.max_columns', None)
 st
 
-//State counts
+#State counts
 st['State'].value_counts()
 
-//District counts
+#District counts
 st['District'].value_counts()
 
-//Classes count
+#Classes count
 st['Class'].value_counts()
 
-//No. of schools
+#No. of schools
 st['Number of schools surveyed'].value_counts()
 
-//Year surveyed
+#Year surveyed
 st['Year'].value_counts()
 
-//First 10 records of !2017
+#First 10 records of !2017
 info1=st[st['YearCode']!=2017]
 info1.head(10)
 
-//First 10 records of !2021
+#First 10 records of !2021
 info2=st[st['YearCode']!=2021]
 info2.head(10)
 
